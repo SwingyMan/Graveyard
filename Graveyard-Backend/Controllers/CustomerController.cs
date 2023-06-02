774cd916b206e72739daa601e4d2c45851b30065
Marcin <asssinowo90@gmail.com>
2023-06-02 11:42:39 +0200

    added getSelf for customer

--- Graveyard-Backend/Controllers/CustomerController.cs
+++ Graveyard-Backend/Controllers/CustomerController.cs
@@ -73,4 +73,11 @@ public class CustomerController : ControllerBase
     {
         return Ok(_customerService.UpdateUser(id, customer));
     }
+
+    [HttpGet]
+    public async Task<IActionResult> getSelf()
+    {
+        return Ok(_customerService.GetUser(int.Parse(User.Claims
+            .First(i => i.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value)));
+    }
 }
