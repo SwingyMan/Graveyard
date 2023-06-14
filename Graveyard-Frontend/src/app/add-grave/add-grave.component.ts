668ec63f458db17b95f9d51ca9ebe16a303fdc9b
Pieroggo <greg2mazur@gmail.com>
2023-06-14 20:41:08 +0200

    mini commit #1

--- Graveyard-Frontend/src/app/add-grave/add-grave.component.ts
+++ Graveyard-Frontend/src/app/add-grave/add-grave.component.ts
@@ -12,11 +12,30 @@ export class AddGraveComponent {
   parentComponent:AppComponent;
   x:number;
   y:number;
+  public getJsonValue: any;
+  public postJsonValue: any;
   constructor(private appComponent: AppComponent, private http: HttpClient, private toastr: ToastrService) {
     this.parentComponent = appComponent;
     this.x=0;
     this.y=0;
   }
-  
-  public addGrave() {console.log("X: "+this.x+" Y: "+this.y)}
+  public checkForCoords(){
+    const header=new HttpHeaders({
+        "Content-Type":"application/json"
+      });
+    let n=0;
+    //this.http.get()
+  }
+  public addGrave() {
+    const httpOptions={
+      headers:new HttpHeaders({
+        contentType: 'application/json',
+      })
+    };
+    let body={
+      x:this.x,
+      y:this.y
+    };
+
+    console.log("X: "+this.x+" Y: "+this.y)}
 }
