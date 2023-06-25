4cb0bd7dc4f56d94ae01fb63e891c6e226fb3fdd
Pieroggo <greg2mazur@gmail.com>
2023-06-25 12:47:46 +0200

    mini commit, preparation for edit/delete of accounts using lists

--- Graveyard-Frontend/src/app/delete-accounts/delete-accounts.component.ts
+++ Graveyard-Frontend/src/app/delete-accounts/delete-accounts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'grv-delete-accounts',
@@ -6,6 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete-accounts.component.css']
 })
 export class DeleteAccountsComponent {
+  //parentComponent:AppComponent;
+  pageToShowAccounts:number=0;
+
+  selectedAccountToEdit:number=0
+
   deletedAccountID:number=0;
+  selectedAccountToDelete:number=0;
+
+  public showEditAccounts(){
+    this.pageToShowAccounts=0;
+  }
+  public showDeleteAccounts(){
+    this.pageToShowAccounts=1;
+  }
+  public editAccount(){}
   public deleteAccount(){}
 }
